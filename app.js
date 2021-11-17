29c759e79af8e9ac537e182ae270a520fe79b30b
wison <303404405@qq.com>
2021-11-17 21:39:07 +0800

    socket逻辑修改

--- app.js
+++ app.js
@@ -7,46 +7,81 @@ const app = new Koa();
 const http = require("http").createServer(app.callback());
 const io = require("socket.io")(http);
 
-const roomInfo = {};
+// const roomInfo = {};
+// io.on("connection", (socket) => {
+//   socket.on("join", (data) => {
+//     console.log("data", data);
+//     const roomID = data.roomID;
+//     const user = data.user;
+//     user.socketID = socket.id;
+//     socket.join(roomID);
+//     if (!roomInfo[roomID]) {
+//       roomInfo[roomID] = [];
+//     }
+//     const inFlag = roomInfo[roomID].find((item) => {
+//       return item.uuid === user.uuid;
+//     });
+//     if (inFlag) return;
+//     roomInfo[roomID].push(user);
+//     console.log("roomInfo[roomID]", roomInfo[roomID]);
+//     io.in(roomID).emit("joined", roomInfo[roomID].length);
+
+//     socket.on("disconnect", () => {
+//       console.log("roomInfo[roomID]", roomInfo[roomID]);
+//       console.log("socket.id", socket.id);
+//       roomInfo[roomID] = roomInfo[roomID].filter((item) => {
+//         return item.socketID !== socket.id;
+//       });
+//       io.in(roomID).emit("leaved", roomInfo[roomID]);
+//     });
+
+//     socket.on('client message',data=>{
+//       const val = roomInfo[roomID].find((item) => {
+//         return item.socketID === socket.id;
+//       });
+//       let msgObj = {
+//         name: val.name,
+//         me: false,
+//         content: data.msg
+//       }
+//       socket.to(roomID).emit('server message',msgObj)
+//     })
+//   });
+// });
+
 io.on("connection", (socket) => {
+  let roomID;
   socket.on("join", (data) => {
-    console.log("data", data);
-    const roomID = data.roomID;
-    const user = data.user;
-    user.socketID = socket.id;
-    socket.join(roomID);
-    if (!roomInfo[roomID]) {
-      roomInfo[roomID] = [];
+    roomID = data.roomID;
+    let myRoom = io.sockets.adapter.rooms.get(roomID)
+    let userNum=0
+    if(myRoom){
+      userNum = +myRoom.size
     }
-    const inFlag = roomInfo[roomID].find((item) => {
-      return item.uuid === user.uuid;
-    });
-    if (inFlag) return;
-    roomInfo[roomID].push(user);
-    console.log("roomInfo[roomID]", roomInfo[roomID]);
-    io.in(roomID).emit("joined", roomInfo[roomID].length);
-
-    socket.on("disconnect", () => {
-      console.log("roomInfo[roomID]", roomInfo[roomID]);
-      console.log("socket.id", socket.id);
-      roomInfo[roomID] = roomInfo[roomID].filter((item) => {
-        return item.socketID !== socket.id;
-      });
-      io.in(roomID).emit("leaved", roomInfo[roomID]);
-    });
-
-    socket.on('client message',data=>{
-      const val = roomInfo[roomID].find((item) => {
-        return item.socketID === socket.id;
-      });
-      let msgObj = {
-        name: val.name,
-        me: false,
-        content: data.msg
-      }
-      socket.to(roomID).emit('server message',msgObj)
-    })
+    if(userNum>=2){
+      socket.emit("fulled");
+    }else{
+      socket.join(roomID);
+      io.in(roomID).emit("joined", userNum+1);
+    }
   });
+  
+  socket.on("disconnect", () => {
+    const myRoom = io.sockets.adapter.rooms.get(roomID)
+    if(myRoom){
+      if(+myRoom.size>=2) return
+      socket.to(roomID).emit("joined", myRoom.size);
+    }
+  });
+
+  socket.on('client message',data=>{
+    let msgObj = {
+      name: data.name,
+      me: false,
+      content: data.msg
+    }
+    socket.to(roomID).emit('server message',msgObj)
+  })
 });
 
 app.use(bodyParser());
